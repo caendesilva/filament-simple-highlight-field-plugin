d65c4356fe9ded164afbf1096d473a59ae003e32
Caen De Silva <caen@desilva.se>
2022-09-03 20:02:52 +0200

    Configure scripts

--- src/FilamentSimpleHighlightFieldServiceProvider.php
+++ src/FilamentSimpleHighlightFieldServiceProvider.php
@@ -7,6 +7,10 @@ use Spatie\LaravelPackageTools\Package;
 
 class FilamentSimpleHighlightFieldServiceProvider extends PluginServiceProvider
 {
+    protected array $scripts = [
+        'filament-simple-highlight-field-scripts' => __DIR__ . '/../resources/assets/highlight.min.js',
+    ];
+
     public function configurePackage(Package $package): void
     {
         $package->name('filament-simple-highlight-field')
