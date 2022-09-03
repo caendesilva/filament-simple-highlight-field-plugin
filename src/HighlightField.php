8c0e76ccd1a43334d9b09a35ee29a86d045b3c74
Caen De Silva <caen@desilva.se>
2022-09-03 19:40:45 +0200

    Create HighlightField.php

---
+++ src/HighlightField.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Desilva\FilamentSimpleHighlightField;
+
+use Filament\Forms\Components\Field;
+
+class HighlightField extends Field
+{
+    protected string $view = 'filament-simple-highlight-field::highlight-field';
+}
