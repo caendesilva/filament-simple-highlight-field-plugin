5d1fd2e425b66290aadddd026314d5755a65a8b5
Caen De Silva <caen@desilva.se>
2022-09-03 20:07:43 +0200

    Configure styles

--- src/FilamentSimpleHighlightFieldServiceProvider.php
+++ src/FilamentSimpleHighlightFieldServiceProvider.php
@@ -7,6 +7,10 @@ use Spatie\LaravelPackageTools\Package;
 
 class FilamentSimpleHighlightFieldServiceProvider extends PluginServiceProvider
 {
+    protected array $styles = [
+        'filament-simple-highlight-field-styles' => __DIR__ . '/../resources/assets/default.min.css',
+    ];
+
     protected array $scripts = [
         'filament-simple-highlight-field-scripts' => __DIR__ . '/../resources/assets/highlight.min.js',
     ];
