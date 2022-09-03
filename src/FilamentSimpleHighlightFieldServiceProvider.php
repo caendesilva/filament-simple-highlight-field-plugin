1ef30284b2eb3eb1681c450a153df4667171fb21
Caen De Silva <caen@desilva.se>
2022-09-03 19:37:01 +0200

    Update FilamentSimpleHighlightFieldServiceProvider.php

--- src/FilamentSimpleHighlightFieldServiceProvider.php
+++ src/FilamentSimpleHighlightFieldServiceProvider.php
@@ -2,24 +2,16 @@
 
 namespace Desilva\FilamentSimpleHighlightField;
 
+use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Desilva\FilamentSimpleHighlightField\Commands\FilamentSimpleHighlightFieldCommand;
 
-class FilamentSimpleHighlightFieldServiceProvider extends PackageServiceProvider
+class FilamentSimpleHighlightFieldServiceProvider extends PluginServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('filament-simple-highlight-field')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_filament-simple-highlight-field_table')
-            ->hasCommand(FilamentSimpleHighlightFieldCommand::class);
+            ->hasViews();
     }
 }
