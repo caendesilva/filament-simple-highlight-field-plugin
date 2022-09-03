d79dfefc88936981d95b331a235ca9dabeee158b
Caen De Silva <caen@desilva.se>
2022-09-03 20:11:16 +0200

    Support dark mode for border

--- resources/views/highlight-field.blade.php
+++ resources/views/highlight-field.blade.php
@@ -15,7 +15,7 @@
         {{ $attributes ->class([
             'bg-white px-3 py-2 rounded-md block w-full transition duration-75 rounded-lg shadow-sm focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600 disabled:opacity-70 filament-forms-textarea-component',
             'dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:border-primary-600' => config('forms.dark_mode'),
-        ])->merge(['style' => 'background: '.(config('forms.dark_mode') ? '#292d3e' : 'white').'; border: 1px solid #dbdee3; font-size: 1.1em;']) }}
+        ])->merge(['style' => 'background: '.(config('forms.dark_mode') ? '#292d3e' : 'white').'; border: 1px solid #'.(config('forms.dark_mode') ? '38414f' : 'dbdee3').'; font-size: 1.1em;']) }}
     >{{ $getState() }}</code></pre>
 </x-dynamic-component>
 
