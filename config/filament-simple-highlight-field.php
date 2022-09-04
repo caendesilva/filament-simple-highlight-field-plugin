3349edf703af103e3ffed99a108830658f55b915
Caen De Silva <caen@desilva.se>
2022-09-04 19:03:45 +0200

    Update config documentation

--- config/filament-simple-highlight-field.php
+++ config/filament-simple-highlight-field.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    // By default, the plugin will load serve the assets from Filament. You can uncomment the following to specify
-    // a custom theme that will then be loaded from the Highlight.js CDN. See https://highlightjs.org/static/demo
+    // This sets the Highlight.js theme to use. The default theme is 'default'.
+    // You can use any theme as it will be loaded from the Highlight.js CDN.
+    // See https://highlightjs.org/static/demo for a list of available themes.
     // 'theme' => 'nord',
 ];
