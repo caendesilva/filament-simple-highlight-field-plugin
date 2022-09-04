6c9bc6c0ff81e4bac7048e47a04dc938ede98e87
Caen De Silva <caen@desilva.se>
2022-09-04 18:57:29 +0200

    Load all assets from CDN

--- src/FilamentSimpleHighlightFieldServiceProvider.php
+++ src/FilamentSimpleHighlightFieldServiceProvider.php
@@ -7,22 +7,10 @@ use Spatie\LaravelPackageTools\Package;
 
 class FilamentSimpleHighlightFieldServiceProvider extends PluginServiceProvider
 {
-    protected array $styles = [
-        'filament-simple-highlight-field-styles' => __DIR__.'/../resources/assets/default.min.css',
-    ];
-
-    protected array $scripts = [
-        'filament-simple-highlight-field-scripts' => __DIR__.'/../resources/assets/highlight.min.js',
-    ];
-
     public function configurePackage(Package $package): void
     {
         $package->name('filament-simple-highlight-field')
             ->hasConfigFile()
             ->hasViews();
-
-        if (config('filament-simple-highlight-field.theme')) {
-            $this->styles = [];
-        }
     }
 }
