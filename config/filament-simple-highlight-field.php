dd99db3916c95709ad2893e3f23113b1b2993683
Caen De Silva <caen@desilva.se>
2022-09-18 12:04:25 +0200

    Update config style

--- config/filament-simple-highlight-field.php
+++ config/filament-simple-highlight-field.php
@@ -1,8 +1,18 @@
 <?php
 
 return [
-    // This sets the Highlight.js theme to use. The default theme is 'default'.
-    // You can use any theme as it will be loaded from the Highlight.js CDN.
-    // See https://highlightjs.org/static/demo for a list of available themes.
+
+    /*
+    |-----------------------------------------------------------------------------
+    | Syntax Highlighting Theme
+    |-----------------------------------------------------------------------------
+    |
+    | This sets the Highlight.js theme to use. The default theme is 'default'.
+    | You can use any theme as it will be loaded from the Highlight.js CDN.
+    | See https://highlightjs.org/static/demo for a list of the themes.
+    |
+    */
+
     // 'theme' => 'nord',
+
 ];
