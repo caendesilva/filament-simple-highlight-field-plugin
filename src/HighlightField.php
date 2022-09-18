b3080027fbef11946bd9322736fdd30ce63c566a
Caen De Silva <caen@desilva.se>
2022-09-18 12:11:47 +0200

    Add helper method to determine if a line is too long to be highlighted

--- src/HighlightField.php
+++ src/HighlightField.php
@@ -7,4 +7,20 @@ use Filament\Forms\Components\Field;
 class HighlightField extends Field
 {
     protected string $view = 'filament-simple-highlight-field::highlight-field';
+
+    public static function canHighlight(string $string): bool
+    {
+        if (config('filament-simple-highlight-field.disable_highlighting_for_compact_files', true)) {
+            $lines = substr_count($string, "\n");
+            $chars = strlen($string);
+
+            $threshold = config('filament-simple-highlight-field.compact_file_line_threshold', 256);
+
+            if (($lines > 0) && ($chars / $lines > $threshold)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
