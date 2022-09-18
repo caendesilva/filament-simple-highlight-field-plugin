1c0a68090e39170264739b26a6dd4d9958b9d875
Caen De Silva <caen@desilva.se>
2022-09-18 12:13:11 +0200

    Extract semantic helper method

--- src/HighlightField.php
+++ src/HighlightField.php
@@ -11,16 +11,21 @@ class HighlightField extends Field
     public static function canHighlight(string $string): bool
     {
         if (config('filament-simple-highlight-field.disable_highlighting_for_compact_files', true)) {
-            $lines = substr_count($string, "\n");
-            $chars = strlen($string);
-
-            $threshold = config('filament-simple-highlight-field.compact_file_line_threshold', 256);
-
-            if (($lines > 0) && ($chars / $lines > $threshold)) {
+            if (static::areLinesTooLongToBeHighlighted($string)) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    protected static function areLinesTooLongToBeHighlighted(string $string): bool
+    {
+        $lines = substr_count($string, "\n");
+        $chars = strlen($string);
+
+        $threshold = config('filament-simple-highlight-field.compact_file_line_threshold', 256);
+
+        return ($lines > 0) && ($chars / $lines > $threshold);
     }
 }
